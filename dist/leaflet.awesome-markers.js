59da8331ea82ecbb35ce02c37e35b441dde83578
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-08-22 14:28:03 +0200

    added double the size markers and made them resizable. working, but kind of hacky

--- dist/leaflet.awesome-markers.js
+++ dist/leaflet.awesome-markers.js
@@ -19,15 +19,29 @@ L.AwesomeMarkers.Icon = L.Icon.extend({
     iconSize: [35, 45], 
     iconAnchor:   [17, 42],
     popupAnchor: [1, -32],
-    shadowAnchor: [10, 12],
+    shadowAnchor: [3, 12],
     shadowSize: [36, 16],
     className: 'awesome-marker',
     icon: 'home',
     color: 'blue',
-    iconColor: 'white'
+    iconColor: 'white',
+    different_size: false,
+    size_factor: 1
   },
 
   initialize: function (options) {
+   if (options.different_size){
+      options.className = 'awesome-marker awesome-marker-double';
+      // everything is double size.
+      options.size_factor = options.size_factor / 2;
+      options.iconSize = [70 * options.size_factor, 
+                          90 * options.size_factor];
+      options.iconAnchor = [34 * options.size_factor, 
+                            92 * options.size_factor];
+      options.shadowAnchor = [10, 12];
+      options.shadowSize = [72 * options.size_factor, 
+                            32 * options.size_factor];
+    }
     options = L.setOptions(this, options);
   },
 
@@ -44,7 +58,31 @@ L.AwesomeMarkers.Icon = L.Icon.extend({
               (-options.bgPos.x) + 'px ' + (-options.bgPos.y) + 'px';
     }
 
-    this._setIconStyles(div, 'icon-' + options.color);
+    if (options.different_size) {
+      div.style.backgroundSize =
+              (options.size_factor * 720) + 'px ' + 
+              (options.size_factor * 92) + 'px' ;
+      positions = {
+        red: '0 0',
+        darkred: (options.size_factor * -360) + 'px 0',
+        orange: (options.size_factor * -72) + 'px 0',
+        green: (options.size_factor * -144) + 'px 0',
+        darkgreen: (options.size_factor * -504) + 'px 0',
+        blue: (options.size_factor * -216) + 'px 0',
+        darkblue: (options.size_factor * -512) + 'px 0',
+        purple: (options.size_factor * -288) + 'px 0',
+        darkpurple: (options.size_factor * -576) + 'px 0',
+        cadetblue: (options.size_factor * -648) + 'px 0'
+      };
+      div.style.backgroundPosition = positions[options.color];
+      div.style.fontSize = (options.size_factor * 28) + 'px';
+    }
+
+    if (!options.different_size){
+      this._setIconStyles(div, 'icon-' + options.color);
+    } else {
+      this._setIconStyles(div);
+    }
     return div;
   },
 
@@ -75,7 +113,11 @@ L.AwesomeMarkers.Icon = L.Icon.extend({
       anchor = size.divideBy(2, true);
     }
 
-    img.className = 'awesome-marker-' + name + ' ' + options.className;
+    if ((options.different_size) && (name == 'shadow')) {
+      img.className = 'awesome-marker-' + name + '-double' +' ' + options.className;
+    } else{
+      img.className = 'awesome-marker-' + name + ' ' + options.className;
+    }
 
     if (anchor) {
       img.style.marginLeft = (-anchor.x) + 'px';
@@ -102,6 +144,4 @@ L.AwesomeMarkers.icon = function (options) {
 };
 
 }(this, document));
-
-
 
